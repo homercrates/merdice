2d1ced8312bee93eb811b3e967436f42019e7b1e
smw mc <smeredithwilliams@gmail.com>
2019-06-28 19:58:49 -0700

    changed images

--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -3,11 +3,15 @@ import React from 'react';
 import RenderTiles from '../components/renderTiles';
 import DiceComponent from '../components/diceComponent';
 
+
 function MainPage() {
     return (
         <div className="App">
           <h1>
-            Dice Fu
+            Dice Fu <img src={require(`../assets/1.png`)} className="tile" alt="fist" height="40vh"/> 
+            > <img src={require(`../assets/2.png`)} className="tile" alt="fist" height="40vh"/> 
+            > <img src={require(`../assets/3.png`)} className="tile" alt="fist" height="40vh"/> 
+            > <img src={require(`../assets/1.png`)} className="tile" alt="fist" height="40vh"/> 
           </h1>
           <div className="DiceBoard">
             <RenderTiles />
