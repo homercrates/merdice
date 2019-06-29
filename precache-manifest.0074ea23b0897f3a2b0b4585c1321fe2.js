debfb317778848e8fd38cbc3213d6e1da5603406
smw mc <smeredithwilliams@gmail.com>
2019-06-28 20:12:06 -0700

    Updates

---
+++ precache-manifest.0074ea23b0897f3a2b0b4585c1321fe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e65b88e38c8679ab8a76ef60f3c94c9",
+    "url": "/dicefu/index.html"
+  },
+  {
+    "revision": "d03519ab836f392c84d0",
+    "url": "/dicefu/static/css/main.03df00fc.chunk.css"
+  },
+  {
+    "revision": "67f36c7b3848e9d2d6b4",
+    "url": "/dicefu/static/js/2.2ed87113.chunk.js"
+  },
+  {
+    "revision": "d03519ab836f392c84d0",
+    "url": "/dicefu/static/js/main.87dabe8f.chunk.js"
+  },
+  {
+    "revision": "df49b9ef254995ba6f32",
+    "url": "/dicefu/static/js/runtime~main.3eac6ed5.js"
+  }
+]);
