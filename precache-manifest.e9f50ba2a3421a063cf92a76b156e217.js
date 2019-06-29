0c2d4fd1bfff1767a589b243159b104ac8763449
smw mc <smeredithwilliams@gmail.com>
2019-06-28 23:37:38 -0700

    Updates

---
+++ precache-manifest.e9f50ba2a3421a063cf92a76b156e217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ea511a269ed3fa8aac0fe5832704bd",
+    "url": "/merdice/index.html"
+  },
+  {
+    "revision": "1e0d6846f56cb1e4a8d5",
+    "url": "/merdice/static/css/main.03df00fc.chunk.css"
+  },
+  {
+    "revision": "67f36c7b3848e9d2d6b4",
+    "url": "/merdice/static/js/2.2ed87113.chunk.js"
+  },
+  {
+    "revision": "1e0d6846f56cb1e4a8d5",
+    "url": "/merdice/static/js/main.aee0d9d8.chunk.js"
+  },
+  {
+    "revision": "9074c9ef7cac564bcb4e",
+    "url": "/merdice/static/js/runtime~main.1a1c9b5d.js"
+  }
+]);
